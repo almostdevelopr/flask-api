acd87f3afcf931beff9c6cae58c1d41ee836c670
almostdevelopr <minhalam.dev@gmail.com>
2022-06-24 21:22:18 +0530

    Add flask api

--- app.py
+++ app.py
@@ -3,9 +3,30 @@ from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET'])  # To render homepage
 def index():
     return render_template('index.html')
+
+
+@app.route('/math', methods=['POST'])  # This will be called from the UI
+def math_operation():
+    if request.method == 'POST':
+        operation = request.form['operation']
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        if operation == 'add':
+            r = num1 + num2
+            result = 'the sum of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        if operation == 'subtract':
+            r = num1 - num2
+            result = 'the difference of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        if operation == 'multiply':
+            r = num1 * num2
+            result = 'the product of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        if operation == 'divide':
+            r = num1 / num2
+            result = 'the quotient of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        return render_template('results.html', result=result)
 
 
 @app.route('/via_postman', methods=['POST'])
@@ -18,17 +39,17 @@ def math_operation_via_postman():
             r = num1 + num2
             result = 'the sum of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
         if operation == 'subtract':
-            r = num1 + num2
+            r = num1 - num2
             result = 'the difference of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
         if operation == 'multiply':
-            r = num1 + num2
+            r = num1 * num2
             result = 'the product of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
         if operation == 'divide':
-            r = num1 + num2
+            r = num1 / num2
             result = 'the quotient of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
         return jsonify(result)
 
 
 if __name__ == "__main__":
-    print("App is running..")
-    app.run()
+    # print("App is running..")
+    app.run(debug=True)
